1d358a8a6960c8a0fe92a88cb01c319d59c73294
shahruk-maharuj <shahrukmaharuj10@gmail.com>
2025-08-26 18:55:42 +0600

    add router

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-function App() {
+function HomePage() {
   return (
     <>
       <h1>Hello From Mernspace</h1>
@@ -6,4 +6,4 @@ function App() {
   );
 }
 
-export default App;
+export default HomePage;
